2bcc817139052641e7ef168bd54cc8fa5e50782d
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-11-06 12:26:56 -0700

    stupid ben

--- register.php
+++ register.php
@@ -1,59 +1,23 @@
-<?php  
- /* set up item prices */
-  @ $db = new mysqli('localhost', 'root', '', 'csmadvice_445');
-  if (mysqli_connect_errno()) {
-     echo 'Error: Could not connect to database.  Please try again later.';
-     exit;
-  }
-
-  $query = "select * from products;";
-  $result = $db->query($query);
-  $num_results = $result->num_rows;
-  
-  $pictures = array(	'coffee1.jpg', 'coffee2.jpg', 'coffee3.jpg',
-						'muffin.jpg', 'muffin2.jpg');
-						
-  shuffle($pictures);
-?>
 <html>
 <head>
 	<title>REGISTER PAGE</title>
 </head>
 <body>
 <h1>REGISTER PAGE</h1>
-<div align="center">
-<table width=100%>
-<tr>
-<?php
-	for($i = 0; $i < 3; $i++){
-		echo "<td align=\"center\"><img src=\"";
-		echo $pictures[$i];
-		echo "\"/></td>";
-	}
-	echo "</tr>";
-	echo "<tr>";
-	echo "<td colspan=\"3\" align=\"center\">";
-	for ($i=0; $i<$num_results; $i++){
-		$row = $result->fetch_assoc();
-		echo '| '.stripslashes($row['name']);
-		$price = stripslashes($row['price']);
-		echo ' | '.number_format($price, 2);
-		echo ' |<br />';	
-	} 
-	echo "</td>";	
-?>
-</tr>
-</table>
-</div>
 
-<form action="processorder.php" method="post">
+<form action="login.php" method="post">
 <table border="0">
 <tr>
-	<td colspan="2" align="left">Your Name <input type="text" name="custname" size="20" maxlength="20" required/></td>
+	<td colspan="2" align="left">First Name <input type="text" name="custname" size="20" maxlength="20" required/></td>
 </tr>
-<tr bgcolor="#cccccc">
-	<td width="200">Item</td>
-	<td width="15">Quantity</td>
+<tr>
+	<td colspan="2" align="left">Last Name <input type="text" name="custname" size="20" maxlength="20" required/></td>
+</tr>
+<tr>
+	<td colspan="2" align="left">Username <input type="text" name="custname" size="20" maxlength="20" required/></td>
+</tr>
+<tr>
+	<td colspan="2" align="left">Password <input type="password" name="custname" size="20" maxlength="20" required/></td>
 </tr>
 <tr>
 	<td>Animal</td>
