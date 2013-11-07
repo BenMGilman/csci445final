1d300102b19dc3d3e817bf2b74b7fd8e6cdbf3f6
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-11-06 23:13:24 -0700

    fixed date problem

--- submitpost.php
+++ submitpost.php
@@ -5,18 +5,15 @@
 		exit;
 	}
 
-	$postQuery = "insert into posts (keyphrase, post, page, post_date, user_id) values (?, ?, ?, ?, ?)";
+	$postQuery = "insert into posts (keyphrase, post, page, post_date, user_id) values (?, ?, ?, CURDATE(), ?)";
 	$postStmt = $db->prepare($postQuery);
 
 	$page = $_POST['page_type'];
 	$post = $_POST['postarea'];
-	// the post_date is always being set to NULL for some reason....
-	// NEED TO FIX not sure if value in database needs to be changed or if there is some way to get the date another way/value
-	$post_date = `getdate()`;	
 	$keyphrase = $_POST['keyphrase'];
 	$user_id = 1;
 	
-	$postStmt->bind_param("ssssi", $keyphrase, $post, $page, $post_date, $user_id);
+	$postStmt->bind_param("sssi", $keyphrase, $post, $page, $user_id);
 	$postStmt->execute();
 	
 	echo '<script type="text/javascript"> document.location.href="'.$page.'page.php"; </script>';
