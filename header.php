82bf6d53b4515bc3d659ba0aa7a32c27048f0f05
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-11-11 16:19:45 -0700

    fixed header for reals!

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 	echo 	'<nav class="navbar">
 				<div id="header">
 					<div id="right">
-						Welcome <?php echo $username; ?> | <a href="userpage.php" style="text-decoration: none">Profile</a> | <a href="index.php" style="text-decoration: none">Sign Out</a>
+						Welcome '; echo $username; 
+						echo '| <a href="userpage.php" style="text-decoration: none">Profile</a> | <a href="index.php" style="text-decoration: none">Sign Out</a>
 					</div>
 					<ul id="pagenav">
 						<li style="list-style-type: none"><a href="homepage.php">Home</a></li>
