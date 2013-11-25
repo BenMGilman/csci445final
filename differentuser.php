262c3fa068699378071ab9d50b7cef528b1ee00f
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-11-25 14:18:06 -0700

    update different users page again

--- differentuser.php
+++ differentuser.php
@@ -10,7 +10,7 @@
 	
 	$lquery = "select * from login where id=\"".$user_id."\";";
 	$lresult = $db->query($lquery);
-	$login = $result->fetch_assoc();
+	$login = $lresult->fetch_assoc();
 	
 	$cquery = "select * from comments where user_id=".$user_id.";";
 	$cresult = $db->query($cquery);
@@ -19,7 +19,6 @@
 	$pquery = "select * from posts where user_id=".$user_id.";";
 	$presult = $db->query($pquery);
 	$num_posts = $presult->num_rows;
-	
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -34,55 +33,56 @@
 // gets the image source from the database
 	echo '<img src="'.$user['photo'].'" alt = "photo" width="140" height="140"/><br>';
 ?>
-<form action="uploadphoto.php" method="post" enctype="multipart/form-data">
-<table border="0">
+<table id ="diffuserinfo">
 	<tr>
 		<td>First Name: </td>
-		<td><input type="text" name="fname" size="18" maxlength="15" value=<?php echo $user['fname']?> /></td>
+		<td><?php echo $user['fname']?></td>
 	</tr>
 	<tr>
 		<td>Last Name: </td>
-		<td><input type="text" name="lname" size="18" maxlength="15" value=<?php echo $user['lname']?> /></td>
+		<td><?php echo $user['lname']?></td>
 	</tr>
 	<tr>
 		<td>Email: </td>
-		<td><input type="email" name="email" size="30" maxlength="30" value=<?php echo $user['email']?> /></td>
+		<td><?php echo $user['email']?></td>
 	</tr>
 	<tr>
 		<td>Status: </td>
-		<td><input type="text" name="status" size="18" maxlength="15" value=<?php echo $user['status']?> /></td>
+		<td><?php echo $user['status']?></td>
 	</tr>
 </table>
-</form>
 <h3>Your Activity:</h3>
 <?php
-	echo '<div id="userposts">';
+	echo '<div id="diffposts">';
 	echo '<h4>Posts:</h4>';
-	if($num_posts < 0)
+	if($num_posts <= 0)
 		echo 'No Recent Posts';
 	else{
 		for ($i=0; $i<$num_posts; $i++){
 			$post = $presult->fetch_assoc();
-			echo '<table><tr><td>';
-			echo stripslashes($post['post_date']).'<br />'.
-				stripslashes($post['post']).'<br />';
-			echo '</td></tr>';
+			echo '<div class="posting">';
+			echo '<table>';
+			echo '<tr><td>'.$post['post_date'].'</td></tr>';
+			echo '<tr><td id="key">'.$post['keyphrase'].'</td></tr>';
+			echo '<tr><td>'.$post['post'].'</td></tr>';
+			echo '</table>';
+			echo '</div>';
 		}
-		echo '</table>';
 	}
 	
 	echo '<h4>Comments:</h4>';
-	if($num_comments < 0)
+	if($num_comments <= 0)
 		echo 'No Recent Comments';
 	else{
 		for ($i=0; $i<$num_comments; $i++){
 			$comment = $cresult->fetch_assoc();
-			echo '<table><tr><td>';
-			echo stripslashes($comment['post_date']).'<br />'.
-				stripslashes($comment['comment']).'<br />';
-			echo '</td></tr>';
+			echo '<div class="posting">';
+			echo '<table>';
+			echo '<tr><td>'.$comment['post_date'].'</td></tr>';
+			echo '<tr><td>'.$comment['comment'].'</td></tr>';
+			echo '</table>';
+			echo '</div>';
 		}
-		echo '</table>';
 	}
 	echo '</div>';
 ?>
